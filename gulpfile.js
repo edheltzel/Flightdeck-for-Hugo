9413e54be5fd1ebf823f9bf66c7c2240222adcd0
Ed Heltzel <ed@rainyday.media>
2022-04-19 16:43:26 -0400

    npm workflow updates - gulp comments and minor chages - adds pnpm-lock

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 'use strict';
 
+// Load plugins
 const { series, parallel, src, dest, watch } = require('gulp');
 const imagemin = require('gulp-imagemin');
 
+// Flightdeck manifest
 const flightdeck = {
   images:{
-    src: './static/images/**/*',
+    src: './assets/images/**/*',
+    dest: './static/images',
     interlaced: false,
     verbose: false,
     mozjpeg:{
@@ -16,6 +19,7 @@ const flightdeck = {
   }
 }
 
+// Optimize Images
 function images(done) {
   src(flightdeck.images.src)
     .pipe(imagemin([
@@ -25,14 +29,16 @@ function images(done) {
       ],
       {verbose: flightdeck.images.verbose}
     ))
-    .pipe(dest(flightdeck.images.src));
+    .pipe(dest(flightdeck.images.dest));
     done();
 }
 
+// Watch files
 function watchFiles() {
   watch(flightdeck.images.src, images);
 }
 
+// export tasks
 exports.images = images;
 exports.watch = watchFiles;
 exports.default = series(images, watchFiles);
